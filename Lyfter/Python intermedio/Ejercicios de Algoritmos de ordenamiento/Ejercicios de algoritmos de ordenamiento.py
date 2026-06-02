e18ce31a6d7e4e663f9dc33b3407cdd740cffb5c
Nina <nina.araya.rojas@gmail.com>
2026-06-02 12:14:47 -0600

    Applied feedback on exercises algoritmos de ordenamiento

--- Lyfter/Python intermedio/Ejercicios de Algoritmos de ordenamiento/Ejercicios de algoritmos de ordenamiento.py
+++ Lyfter/Python intermedio/Ejercicios de Algoritmos de ordenamiento/Ejercicios de algoritmos de ordenamiento.py
@@ -8,12 +8,12 @@ for i in range(len(numbers)):
 
 print(numbers)
 
-#Ejercicio de ordenamiento de burbuja modificado para ordenar de mayor a menor
+#Ejercicio de ordenamiento de burbuja modificado para ordenar derecha a izquierda
 numbers = [8, 2, 6, 4, 10]
 
 for i in range(len(numbers)):
-    for j in range(len(numbers) - 1):
-        if numbers[j] < numbers[j + 1]:
-            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    for j in range(len(numbers) - 1, 0, -1):
+        if numbers[j - 1] > numbers[j]:
+            numbers[j - 1], numbers[j] = numbers[j], numbers[j - 1]
 
 print(numbers)
