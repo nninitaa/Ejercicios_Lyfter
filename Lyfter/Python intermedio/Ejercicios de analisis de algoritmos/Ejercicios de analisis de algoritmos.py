586a8ad0ffb41c24711af76cc7afe6badf2e4dcb
Nina <nina.araya.rojas@gmail.com>
2026-06-04 18:29:02 -0600

    Added ejercicios de analisis de algoritmos

---
+++ Lyfter/Python intermedio/Ejercicios de analisis de algoritmos/Ejercicios de analisis de algoritmos.py
@@ -0,0 +1,46 @@
+#Ejercicio normal de ordenamiento de burbuja
+
+numbers = [8, 2, 6, 4, 10]
+
+for i in range(len(numbers)): #O(n^2) Hay varios ciclos anidados, por lo que se multiplican sus tiempos de ejecución
+    for j in range(len(numbers) - 1): #O(n^2) El ciclo se ejecuta n veces n * n = n^2
+        if numbers[j] > numbers[j + 1]: #O(1) Unicamente se compara dos elementos
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j] #O(1) Unicamente se intercambian dos elementos
+
+print(numbers) #O(1) Esto va a imprimir la lista ordenada de menor a mayor
+
+#Ejercicio de ordenamiento de burbuja modificado para ordenar derecha a izquierda
+numbers = [8, 2, 6, 4, 10]
+
+for i in range(len(numbers)): #O(n^2) Hay varios ciclos anidados
+    for j in range(len(numbers) - 1, 0, -1): #O(n^2) El ciclo se ejecuta n veces n * n = n^2
+        if numbers[j - 1] > numbers[j]: #O(1) Unicamente se compara dos elementos
+            numbers[j - 1], numbers[j] = numbers[j], numbers[j - 1] #O(1) Unicamente se intercambian dos elementos
+
+print(numbers) #O(1) Esto imprime la lista y lee el codigo de derecha a izquierda, pero el resultado es el mismo que el ejercicio anterior
+
+def print_numbers_times_2(numbers_list): 
+	for number in numbers_list:  #O(1) Ya que solamente lee cada numero de la lista dos veces
+		print(number * 2) #O(1) Se imprime ese mismo numero multiplicado por 2
+
+def check_if_lists_have_an_equal(list_a, list_b): 
+	for element_a in list_a: #O(n^2) El ciclo se ejecuta n veces n * n = n^2
+		for element_b in list_b: #O(n^2) El ciclo se ejecuta n veces n * n = n^2
+			if element_a == element_b: #O(1) Unicamente se compara dos elementos
+				return True #O(1) Retorna verdadero si encuentra un elemento igual en ambas listas
+				
+	return False #O(1) Retorna falso si no encuentra un elemento igual en 1ambas listas
+
+def print_10_or_less_elements(list_to_print): 
+	list_len = len(list_to_print) #O(n) Lee la lista para obtener su longitud
+	for index in range(min(list_len, 10)): #O(1) Se limita a imprimir solo los primeros 10 elementos, por lo que el ciclo se ejecuta 10 veces  
+		print(list_to_print[index])#O(1) Imprime el elemento en la posición indice de la lista
+
+def generate_list_trios(list_a, list_b, list_c): 
+	result_list = [] #O(1) Se crea una lista vacia para almacenar los resultados
+	for element_a in list_a: #O(n^2) El ciclo se ejecuta n veces n * n = n^2
+		for element_b in list_b: #O(n^2) El ciclo se ejecuta n veces n * n = n^2
+			for element_c in list_c: #O(n^2) El ciclo se ejecuta n veces n * n = n^2	
+				result_list.append(f'{element_a} {element_b} {element_c}') #O(1) Se agregan los elementos de las tres listas separados por un espacio
+				
+	return result_list #O(1) Retorna la lista 
